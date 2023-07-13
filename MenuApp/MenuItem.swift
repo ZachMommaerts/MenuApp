13e5ff41022eaa6ff235bc377e80ea6edc6b2f9e
ZachMommaerts <zmommaerts@gmail.com>
2023-07-13 16:15:58 -0500

    Create MenuItem file

---
+++ MenuApp/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  MenuApp
+//
+//  Created by Zach Mommaerts on 7/13/23.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name:String
+    var price:String
+    var imageName:String
+}
