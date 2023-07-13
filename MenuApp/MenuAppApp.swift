eacc334e30734f9f6385c7c1fd6986a746084bd2
ZachMommaerts <zmommaerts@gmail.com>
2023-07-13 15:50:43 -0500

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Zach Mommaerts on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
